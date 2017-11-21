cd995a5056fe427ae4a7ce09fc60c1630fe0a63d
Brian Farnhill <brfarn@microsoft.com>
2017-11-21 16:02:16 +1100

    Added webapp resource

---
+++ generators/webapp/index.js
@@ -0,0 +1,93 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      chalk.blue('                          A                       \n') +
+        chalk.blue('                       AAAA                       \n') +
+        chalk.blue('                     AAAAAA AAA                   \n') +
+        chalk.blue('                  AAAAAAA  AAAAA                  \n') +
+        chalk.blue('                AAAAAAAA  AAAAAAA                 \n') +
+        chalk.blue('              AAAAAAAAA  AAAAAAAAA                \n') +
+        chalk.blue('            AAAAAAAAAA  AAAAAAAAAAA               \n') +
+        chalk.blue('           AAAAAAAAAA  AAAAAAAAAAAAAA             \n') +
+        chalk.blue('          AAAAAAAAAA  AAAAAAAAAAAAAAAA            \n') +
+        chalk.blue('         AAAAAAAAAA   AAAAAAAAAAAAAAAAA           \n') +
+        chalk.blue('        AAAAAAAAAA      AAAAAAAAAAAAAAAA          \n') +
+        chalk.blue('      AAAAAAAAAAA         AAAAAAAAAAAAAAA         \n') +
+        chalk.blue('     AAAAAAAAAAA            AAAAAAAAAAAAAA        \n') +
+        chalk.blue('    AAAAAAAAAAA               AAAAAAAAAAAAAA      \n') +
+        chalk.blue('   AAAAAAAAAAA                 AAAAAAAAAAAAAA     \n') +
+        chalk.blue('   AAAAAAAAAA                 AAAAAAAAAAAAAAAA    \n') +
+        chalk.blue('                  AAAAAAAAAAAAAAAAAAAAAAAAAAAAA   \n') +
+        chalk.blue('               AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA   \n') +
+        '\n\nWelcome to the Azure ARM Template project generator for web apps!\n'
+    );
+
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the web app?',
+        validate: function(input) {
+          if (input !== '') {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Where should the resource be created?',
+        default: '[resourceGroup().location]'
+      },
+      {
+        type: 'input',
+        name: 'appHostingPlan',
+        message: 'What is the name of the app hosting plan that will run this site?',
+        validate: function(input) {
+          if (input !== '') {
+            return true;
+          }
+          return false;
+        }
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    var templatePath = this.destinationPath('azuredeploy.json');
+    var template = this.fs.readJSON(templatePath);
+    template = this._addResource(template, this.props);
+    this.fs.writeJSON(templatePath, template, null, 2);
+  }
+
+  _addResource(template, properties) {
+    var newResource = {
+      type: 'Microsoft.Web/sites',
+      kind: 'app',
+      name: properties.name,
+      apiVersion: '2016-08-01',
+      location: properties.location,
+      scale: null,
+      properties: {
+        enabled: true,
+        serverFarmId:
+          "[resourceId('Microsoft.Web/serverfarms', '" + properties.appHostingPlan + "')]"
+      },
+      dependsOn: []
+    };
+
+    template.resources.push(newResource);
+    return template;
+  }
+};
